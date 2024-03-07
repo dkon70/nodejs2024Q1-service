c2249f7aaea6a99a7d27b04ed14c20f5d4651e5d
dkon70 <dkon70@inbox.ru>
2024-03-08 03:20:35 +0700

    feat: implemented track

---
+++ src/track/dto/create-track.dto.ts
@@ -0,0 +1,17 @@
+import { IsNumber, IsString, IsOptional } from "class-validator";
+
+export class CreateTrackDto {
+  @IsString()
+  name: string
+
+  @IsNumber()
+  duration: number
+
+  @IsOptional()
+  @IsString()
+  artistId: string | null
+
+  @IsOptional()
+  @IsString()
+  albumId: string | null
+}
