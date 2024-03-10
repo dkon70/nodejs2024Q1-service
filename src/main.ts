3c71778fac8b59e4984971ad4faafb3fc239fb88
dkon70 <dkon70@inbox.ru>
2024-03-11 04:29:20 +0700

    feat: doc fixed

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import * as YAML from 'yamljs';
+import { SwaggerModule } from '@nestjs/swagger';
+
+const yaml = YAML.load('doc/api.yaml');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  SwaggerModule.setup('doc', app, yaml);
   await app.listen(process.env.PORT);
 }
 bootstrap();
